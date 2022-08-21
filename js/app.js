17c89b30d902bf7c2e771ee38bb6f336af981321
aminulhaque98 <aminulhaque419@gmail.com>
2022-08-21 20:48:09 +0530

    finished

--- js/app.js
+++ js/app.js
@@ -28,6 +28,10 @@ document.getElementById('btn-generet-pin').addEventListener('click', function ()
 });
 
 
+
+
+
+
 document.getElementById('calculator').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const typeNumberField = document.getElementById('typed - numbers');
@@ -52,3 +56,39 @@ document.getElementById('calculator').addEventListener('click', function (event)
     }
 
 })
+
+
+document.getElementById('varify-pin').addEventListener('click', function () {
+
+    const inputFieldDisplay = document.getElementById('display-pin');
+    const currentPin = inputFieldDisplay.value;
+
+
+    const typeNumberField = document.getElementById('typed - numbers');
+    const priviousTypedNumber = typeNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailurMessenge = document.getElementById('pin-failure');
+
+    if (currentPin === priviousTypedNumber) {
+
+        pinSuccessMessage.style.display = "block"
+        pinFailurMessenge.style.display = 'none'
+    }
+    else {
+
+        pinFailurMessenge.style.display = 'block'
+        pinSuccessMessage.style.display = "none"
+    }
+
+
+})
+
+// mcq quesion
+// Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9)
+
+// '2spiderman9873'
+
+
+// return Math.floor(Math.random()*90000) + 10000;
+// get-5digits
